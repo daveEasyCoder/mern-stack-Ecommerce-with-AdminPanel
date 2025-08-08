319ac84cfb6ba294597ceed8e218de3a9fcbada4
Dawit Tiruneh <dawittechguru@gmail.com>
2025-08-08 13:25:40 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://react-fronted-ecommerce.vercel.app/',
   credentials: true 
 }));
 app.use(express.json())
